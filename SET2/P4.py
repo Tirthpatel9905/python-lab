7f71868c74d333eee527c1bab0b40d5fa331aae0
Tirthpatel9905 <tirthmanish99@gmail.com>
2025-02-11 08:24:58 +0530

    add SET1,SET2 add p1.py fahrenheit and celsius add p2.py add p3.py add p4.py add p5.py add p6.py add p7.py add p8.py add p9.py add p10.py add SET2 add p1.py bank account detial add p2.py add p3.py add p4.py

--- SET2/P4.py
+++ SET2/P4.py
@@ -1,1 +1,30 @@
+import csv
 
+def calculate_average(csv_file, column_name):
+    try:
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            if column_name not in reader.fieldnames:
+                print(f"Column '{column_name}' not found.")
+                return None
+
+            total, count = 0, 0
+            for row in reader:
+                try:
+                    total += float(row[column_name])
+                    count += 1
+                except ValueError:
+                    print(f"Skipping row {reader.line_num}: Invalid value.")
+
+            return total / count if count else None
+
+    except FileNotFoundError:
+        print(f"File '{csv_file}' not found.")
+        return None
+
+csv_file_path = 'file.csv'
+column_to_calculate = 'ENGLISH'
+
+result = calculate_average(csv_file_path, column_to_calculate)
+if result is not None:
+    print(f"Average value in '{column_to_calculate}': {result}")
